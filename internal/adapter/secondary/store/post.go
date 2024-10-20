46df8505ab2986934c295bae3cb4e3e7a2974a81
0azis <test@mail.ru>
2024-10-20 15:44:47 +0300

    fix little bug

--- internal/adapter/secondary/store/post.go
+++ internal/adapter/secondary/store/post.go
@@ -44,7 +44,7 @@ func (p post) GetAll() ([]domain.UserPost, error) {
 
 func (p post) GetOne(postID int) (domain.UserPost, error) {
 	var post domain.UserPost
-	err := p.db.Get(&post, `select posts.id, title, date, preview, username, name, avatar, content from posts inner join users on posts.user_id = users.id where post_id = ? and public = 1`, postID)
+	err := p.db.Get(&post, `select posts.id, title, date, preview, username, name, avatar, content from posts inner join users on posts.user_id = users.id where posts.id = ? and public = 1`, postID)
 	// rows, err := p.db.Query(`select posts.id, title, date, preview, username, name, avatar, content from posts inner join users on posts.user_id = users.id where posts.id = ? and public = 1`, postID)
 	if err != nil {
 		return post, err
